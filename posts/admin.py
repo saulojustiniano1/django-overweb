3ea6e0a99d64571690b7b1a7b64ad2bfb7f82fba
saulojustiniano1 <saulojustiniano1@gmail.com>
2024-04-08 22:44:39 -0300

    feat: aplicacao - like | comment
    Adicionado a aplicação de likes e comments no projeto

--- posts/admin.py
+++ posts/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
 
-from posts import models
+from .models import *  # Post, Comment, Like
+
+# from models import Post
 
 
-@admin.register(models.Post)
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title",)
-    # filter_horizontal = ("title",)
+    # filter_horizontal = ("title", "content", "pub_date")
     search_fields = ("content",)
     list_filter = ("title",)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("post",)
+    # filter_horizontal = ("title", "content", "pub_date")
+    search_fields = ("content",)
+    list_filter = ("post",)
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ("post",)
+    # filter_horizontal = ("title", "content", "pub_date")
+    search_fields = ("post",)
+    list_filter = ("post",)
+
+
+# admin.site.register(Comment)
+# admin.site.register(Post)
--- posts/models.py
+++ posts/models.py
@@ -5,3 +5,24 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.content}'
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.post}'

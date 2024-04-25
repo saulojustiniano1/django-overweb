aaab1ad45c94ee5c1c5ef1666bb7ea5342e49938
saulojustiniano1 <saulojustiniano1@gmail.com>
2024-04-25 10:17:25 -0300

    fix: Refactor templates and static files structure, update CSS files, and fix static file paths
    Refatore modelos e estrutura de arquivos estáticos, atualize arquivos CSS
    e corrija caminhos de arquivos estáticos.

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
@@ -13,11 +14,18 @@ def home(request):
     return render(request, 'posts/home.html')
 
 
-class PostCreateView(CreateView):
+class PostCreateView(PermissionRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content', 'image']
     template_name = 'posts/post_create.html'
     success_url = '/posts/'
+    permission_required = "posts.add_post"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pertence_ao_grupo'] = self.request.user.groups.filter(
+            name='MOD').exists()
+        return context
 
 
 class PostDetailView(DetailView):
@@ -38,10 +46,26 @@ class PostDeleteView(DeleteView):
     success_url = '/posts/'
 
 
-class PostListView(ListView):
+class PostListView(LoginRequiredMixin, ListView):
+    login_url = 'login'
     model = Post
     template_name = 'posts/post_list.html'
     context_object_name = 'posts'
+    paginate_by = 5
+
+    def dispatch(self, request, *args, **kwargs) -> HttpResponse:
+        if self.request.user and not self.request.user.groups.filter(name='USER'):
+            return redirect('login')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(title__icontains=query)
+            print(queryset.query)
+        orderby = self.request.GET.get('orderby', '-pub_date')
+        return queryset.order_by(orderby)
 
 
 class CustomLoginView(LoginView):
@@ -56,15 +80,3 @@ class CustomRegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'auth/register.html'
     success_url = '/login/'
-
-
-class PostListView(LoginRequiredMixin, ListView):
-    login_url = 'login'
-    model = Post
-    template_name = 'posts/post_list.html'
-    context_object_name = 'posts'
-
-    # def dispatch(self, request, *args, **kwargs) -> HttpResponse:
-    #     if self.request.user and not self.request.user.groups.filter(name='editor'):
-    #         return redirect('login')
-    #     return super().dispatch(request, *args, **kwargs)
